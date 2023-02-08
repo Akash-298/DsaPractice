63bbda630a09d741dfb9961995ea016aa5d017d5
Akash-298 <akash.c.c298@gmail.com>
2023-02-08 19:07:56 +0530

    Queue

---
+++ long Queue.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        callWala(a, n);
+
+    }
+
+    public static void callWala(int[] a, int n) {
+
+        int ct = 1;
+        int q = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                ct++;
+            }
+        }
+        System.out.println(ct);
+    }
+}
