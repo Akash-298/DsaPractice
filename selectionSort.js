591515e2c21727a9785b922a5588b4cb91d9be3f
AKASH CHIKHALONDE <105929312+Akash-298@users.noreply.github.com>
2023-07-29 23:41:57 +0530

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
